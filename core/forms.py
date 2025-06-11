e1c84c44491eef8609e984d8d68bccaece3a7a95
cjkreates <105925646+Cjkreates@users.noreply.github.com>
2025-06-11 17:14:08 +0300

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,19 +1,21 @@
 from django import forms
-from .models import Income,Expense
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from .models import Income, Expense
+
 
 class Incomeform(forms.ModelForm):
-	class Meta:
-		model = Income
-		fields = ['source','amount','date','description']
+    class Meta:
+        model = Income
+        fields = ['source', 'amount', 'date', 'description']
 
 
 class Expenseform(forms.ModelForm):
-	class Meta:
-		model = Expense
-		fields = ['category','amount','date','description']
+    class Meta:
+        model = Expense
+        fields = ['category', 'amount', 'date', 'description']
+
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
